996f5fa39c5329bfd7b434bd7b87b58bf3715eab
9Akshit1 <129630861+9Akshit1@users.noreply.github.com>
2024-09-18 20:20:33 -0400

    Update Surge_Stitch_2.ino to replace stepper motor with GEAR motor

--- Surge_Stitch_2.ino
+++ Surge_Stitch_2.ino
@@ -6,44 +6,36 @@
 #define checkbit(x,y)((x)&(y))   //Define macro to checking the bit of register
 #include <LCD.h>                  //include LCD library
 
-int c = 0;
-int d = 0;
 int main(void)              
 {
-  DDRB=0xFF;
+  DDRB=0xF0;
   DDRD=0xF0; 
-  //Note: a stepper motor's step is about pi/24 mm for distance since a stepper motor takes 48 steps fro fool revolution andthe diameter of axel is 2 mm             
-  int currentX = 1430;         // Current position of the stepper motor in steps (range is 0 to 1436 steps)  //we start here, because stepper motor will bring the track set towards it sicne it does nto the have the ability to push it, it will jsut pull it fro mbehind
-  int currentY = 0;          // Current position of the DC motor in stepper motor steps units   (range is 0 to 947 steps)
-  int motorSpeed = 100;     // Speed of the DC motor (units per second)     //tr 350, 178, adn 140000 RPM                  --------- change prob  
-  int duration = 0;
+  int currentX = 255;         // Current position of the GEAR motor in mm (the stage is all the way to the right)
+  int currentY = 0;          // Current position of the DC motor in mm (this motor is at the bottom of its axis) (170 mm is full upwards)
+  int motorSpeed1 = 10;     // Speed of the DC GEAR motor (units per second)     //tr 10, 350, 178, adn 140000 RPM                  --------- change prob  
+  int motorSpeed2 = 100;     // Speed of the DC motor (units per second)     //tr 350, 178, adn 140000 RPM                  --------- change prob  
+  int deltaX, deltaY, duration = 0;
   while(1)
     {
-      int targetX = 100;
-      int targetY = 900;
+      int targetX = 0;    //full left
+      int targetY = 170;  //full up
       
-      deltaX = currentX - targetX
-      if (deltaX < 0)
-        {
-          for(int i=0; i<abs(deltaX); i++)
-          {
-            c++;
-            antistepper();
-            _delay_ms(50);
-          }
-        }
-      else
-        {
-          for(int i=0; i<deltaX; i++)
-          {
-            d++;
-            clkstepper();
-            _delay_ms(50);
-          }
-        }
+      deltaX = currentX - targetX;   //We will normally go from right to left (essentially pulling the stage towards motor)
+      duration = abs(deltaX) / motorSpeed1 * 1000;  // Convert to milliseconds
+      if (deltaX > 0) 
+      {
+        PORTD = 0x40;        //PD7 is LOW. PD6 is HIGH. Anticlockwise. Leftward X-movement
+      } 
+      else 
+      {
+        PORTD = 0x80;       //PD7 is HIGH. PD6 is LOW. Clockwise. Rightward X-movement
+      }
+      _delay_ms(duration);  // Move for the calculated duration
+      // Stop the DC GEAR motor
+      PORTD = 0x00;     //PD7,6 are LOW. Stop motors, hopefully no inertia.
     
-      deltaY = currentY - targetY
-      duration = abs(deltaY) / motorSpeed * 1000;  // Convert to milliseconds
+      deltaY = currentY - targetY;   //We will go up and down depending on the borders of the wound
+      duration = abs(deltaY) / motorSpeed2 * 1000;  // Convert to milliseconds
       if (deltaY < 0) 
       {
         PORTB = 0x40;        //PB7 is LOW. PB6 is HIGH. Anticlockwise. Downward Y-movement
@@ -54,53 +46,11 @@ int main(void)
       }
       _delay_ms(duration);  // Move for the calculated duration
       // Stop the DC motor
-      PORTB = 0x00;     //PB7,6 are LOW. No movement
+      PORTB = 0x00;     //PB7,6 are LOW. Stop motors, hopefully no inertia.
 
       //Relay control for solenoid
       PORTB = 0x20;        //PB5(relay) is HIGH meaning Solenoid Extends
-      _delay_ms(50);
+      _delay_ms(100);                                                           //------change prob
       PORTB = 0x00;        //PB5(relay) is LOW meaning Solenoid Retracts
     }
 }
-
-void antistepper()
-  {
-    if (c==1)
-        {
-          PORTD=0xE0;
-        }
-    if (c==2)
-        {
-          PORTD=0xD0;
-        }
-    if (c==3)
-        {
-          PORTD=0xB0;
-        }
-    if (c==4)
-        {
-          PORTD=0x70;
-          c=0;
-        }
-  }
-
-void clkstepper()
-  {
-    if (d==1)
-        {
-          PORTD=0x70;
-        }
-    if (d==2)
-        {
-          PORTD=0xB0;
-        }
-    if (d==3)
-        {
-          PORTD=0xD0;
-        }
-    if (d==4)
-        {
-          PORTD=0xE0;
-          d=0;
-        }
-  }
